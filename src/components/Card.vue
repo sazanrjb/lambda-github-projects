689346bf562963b2b691d29fc23b29604dd9d6a3
Sajan Rajbhandari <sazanrjb@gmail.com>
2019-02-09 14:54:32 +0545

    List Repos

---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">{{ data.name }}</p>
+      <a href="#" class="card-header-icon" aria-label="more options">
+        <span class="icon">
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </span>
+      </a>
+    </header>
+    <div class="card-content">
+      <div class="content">
+        {{ data.description }}
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Save</a>
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: {
+      data: {
+          type: Object,
+          required: true,
+      },
+  },
+};
+</script>
+
+<style scoped>
+    .card {
+        min-height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .card-content {
+        flex-grow: 1;
+        flex: 1,
+    }
+    .card-header {
+        flex-shrink: 1;
+    }
+</style>
+
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,128 +1,161 @@
 <template>
-    <div class="container">
-        <section class="hero is-light">
-            <div class="hero-body">
-                <p>Enter GitHub Username</p>
-                <h2 class="subtitle">
-                    <div class="field has-addons">
-                        <div class="control is-expanded" :class="{ 'is-loading': usernameLoading }">
-                            <input type="text" class="input" placeholder="Username" v-model="username" @keydown.enter="searchUser"><br><br>
-                        </div>
-                        <div class="control">
-                            <button class="button is-info" @click="searchUser">
-                            Search
-                            </button>
-                        </div>
-                    </div>
-                    <p v-if="errorBag.username">{{ errors.username }}</p>
-                </h2>
+  <div class="container">
+    <section class="hero is-light">
+      <div class="hero-body">
+        <p>Enter GitHub Username</p>
+        <h2 class="subtitle">
+          <div class="field has-addons">
+            <div class="control is-expanded" :class="{ 'is-loading': usernameLoading }">
+              <input
+                type="text"
+                class="input"
+                placeholder="Username"
+                v-model="username"
+                @keydown.enter="searchUser"
+              >
+              <br>
+              <br>
             </div>
-        </section>
-
-        <div v-if="user.id">
-            <div class="content-header has-text-centered">
-                <div class="is-flex is-horizontal-center">
-                    <figure class="image is-128x128">
-                        <img class="is-rounded" :src="user.avatar_url" :alt="user.name" width="150px" height="150px">
-                    </figure>
-                </div>
-                <h2>{{user.name}}</h2>
-
-                <p><a :href="user.html_url" target="_blank" rel="noopener noreferrer">@{{ user.login }}</a></p>
-                <p><a :href="user.blog">{{ user.blog }}</a></p>
-                <p>Joined At: {{ joinedAt }}</p>
+            <div class="control">
+              <button class="button is-info" @click="searchUser">Search</button>
             </div>
+          </div>
+          <p v-if="errorBag.username">{{ errors.username }}</p>
+        </h2>
+      </div>
+    </section>
 
-            <nav class="box level is-mobile">
-                <div class="level-item has-text-centered">
-                    <div>
-                    <p class="heading">Public Repos</p>
-                    <p class="title">{{ user.public_repos }}</p>
-                    </div>
-                </div>
-                <div class="level-item has-text-centered">
-                    <div>
-                    <p class="heading">Public Gists</p>
-                    <p class="title">{{ user.public_gists }}</p>
-                    </div>
-                </div>
-                <div class="level-item has-text-centered">
-                    <div>
-                    <p class="heading">Followers</p>
-                    <p class="title">{{ user.followers }}</p>
-                    </div>
-                </div>
-                <div class="level-item has-text-centered">
-                    <div>
-                    <p class="heading">Following</p>
-                    <p class="title">{{ user.following }}</p>
-                    </div>
-                </div>
-            </nav>
+    <div v-if="user.id" class="content">
+      <div class="has-text-centered">
+        <div class="is-flex is-horizontal-center">
+          <figure class="image is-128x128">
+            <img
+              class="is-rounded"
+              :src="user.avatar_url"
+              :alt="user.name"
+              width="150px"
+              height="150px"
+            >
+          </figure>
         </div>
+        <h2>{{user.name}}</h2>
+
+        <p>
+          <a :href="user.html_url" target="_blank" rel="noopener noreferrer">@{{ user.login }}</a>
+        </p>
+        <p>
+          <a :href="user.blog">{{ user.blog }}</a>
+        </p>
+        <p>Joined At: {{ joinedAt }}</p>
+      </div>
+
+      <nav class="box level is-mobile">
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Public Repos</p>
+            <p class="title">{{ user.public_repos }}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Public Gists</p>
+            <p class="title">{{ user.public_gists }}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Followers</p>
+            <p class="title">{{ user.followers }}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Following</p>
+            <p class="title">{{ user.following }}</p>
+          </div>
+        </div>
+      </nav>
+
+      <div v-if="hasRepos">
+            <div class="columns is-mobile is-multiline">
+                <card class="column" 
+                    v-for="repo in Object.keys(repos)" 
+                    :data="repos[repo]" 
+                    :key="repos[repo].id"
+                ></card>
+            </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-import { format } from 'date-fns';
+import axios from "axios";
+import { format } from "date-fns";
+import Card from './Card.vue';
 
 export default {
-
-    name: 'Dashboard',
-    computed: {
-        errorBag() {
-            return this.errors;
-        },
-        joinedAt() {
-            return format(this.user.created_at, 'MMMM Do, YYYY');
-        },
+  name: "Dashboard",
+  computed: {
+    errorBag() {
+      return this.errors;
     },
-    data() {
-        return {
-            username: '',
-            usernameLoading: false,
-            errors: {},
-            user: {},
-            repos: [],
-        };
+    joinedAt() {
+      return format(this.user.created_at, "MMMM Do, YYYY");
     },
-    methods: {
-        searchUser() {
-            if (!this.username) return;
-            this.usernameLoading = true;
+    hasRepos() {
+      return !!Object.keys(this.repos).length;
+    }
+  },
+  components: {
+      Card,
+  },
+  data() {
+    return {
+      username: "",
+      usernameLoading: false,
+      errors: {},
+      user: {},
+      repos: {}
+    };
+  },
+  methods: {
+    searchUser() {
+      if (!this.username) return;
+      this.usernameLoading = true;
 
-            axios.post('.netlify/functions/user', { username: this.username })
-            .then(({ data }) => {
-                this.errors = {};
-                this.usernameLoading = false;
-                this.user = Object.assign({}, data);
+      axios
+        .post(".netlify/functions/user", { username: this.username })
+        .then(({ data }) => {
+          this.errors = {};
+          this.usernameLoading = false;
+          this.user = Object.assign({}, data);
 
-                this.fetchRepos(this.username);
-            })
-            .catch(({ response }) => {
-                this.user = {};
-                this.usernameLoading = false;
-                this.$set(this.errors, 'username', response.data.message);
-            });
-        },
-        fetchRepos(username) {
-            axios.post('.netlify/functions/repos', { username: this.username })
-            .then(({ data }) => {
-                this.repos = Object.assign({}, data);
-            })
-            .catch(({ response }) => {
-            });
-        },
+          this.fetchRepos(this.username);
+        })
+        .catch(({ response }) => {
+          this.user = {};
+          this.usernameLoading = false;
+          this.$set(this.errors, "username", response.data.message);
+        });
     },
-}
+    fetchRepos(username) {
+      axios
+        .post(".netlify/functions/repos", { username: this.username })
+        .then(({ data }) => {
+          this.repos = Object.assign({}, data);
+        })
+        .catch(({ response }) => {});
+    }
+  }
+};
 </script>
 
 <style scoped>
-    .is-horizontal-center {
-      justify-content: center;
-    }
-    .content-header {
-        margin: 20px;
-    }
+.is-horizontal-center {
+  justify-content: center;
+}
+.content {
+  margin: 20px;
+}
 </style>
